b18ab460d07bd0652751c121e9c3b185b4768028
Tamanna <effat.tamanna@savethechildren.org>
2023-05-24 13:32:09 +0600

    007 Input Aliases

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
-  @Input() postImg = ''
+  @Input('image') postImg = ''
 
 }

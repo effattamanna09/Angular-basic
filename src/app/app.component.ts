7902215aff77fa204d60b7e942692366f4275304
Tamanna <effat.tamanna@savethechildren.org>
2023-05-24 12:20:50 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template : `<h1> hello world </h1>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'basic';
+  title = 'Angular Basic';
+  changeImage(e: KeyboardEvent) {
+    this.imgURl = (e.target as HTMLInputElement).value
+  }
+  imgURl = 'https://www.shiftnow.org/storage/photos/1/teamMemberCarousel/tTI9TXeTyviKHHqeCzHboinnM8UJF6YGUzqbmR55.png'
+  getName() {
+    return this.title;
+  }
+  sayMessage() {
+    alert("hello")
+  }
 }
